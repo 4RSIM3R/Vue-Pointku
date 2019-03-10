e51cf173826b6e485a6422d8ed7bac932bf57abc
Cak Suku <ilzammulkhaq85@gmail.com>
2019-03-10 15:30:55 +0700

    Menambahkan Feature Scan qrcode

---
+++ src/components/Scan.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <qrcode-stream @decode="onDecode"></qrcode-stream>
+  </div>
+</template>
+
+<script>
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+export default {
+name : 'Scan',
+methods: {
+  onDecode (decodedString) {
+    console.log(decodedString)
+  }
+},
+components: {
+    QrcodeStream,
+    QrcodeDropZone,
+    QrcodeCapture
+}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
+import VueQrcodeReader from 'vue-qrcode-reader'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
-
+Vue.use(VueQrcodeReader)
 new Vue({
   router,
   render: h => h(App)
